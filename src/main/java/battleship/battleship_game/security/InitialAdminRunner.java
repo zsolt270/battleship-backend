80c26227a7adb6f9736d005ebb92d8eec8c236b8
zsolt270 <zsolt270000@gmail.com>
2025-08-26 22:41:32 +0200

    Spring security configuration
    -deleted empty line betwenn imports

--- src/main/java/battleship/battleship_game/security/InitialAdminRunner.java
+++ src/main/java/battleship/battleship_game/security/InitialAdminRunner.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
 import java.time.LocalDateTime;
 
 @Component
